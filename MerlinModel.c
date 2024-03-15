90e9deec90ece2df7cef55fa4dcf530e7d150b52
bobpattison123 <ro.pattison123@gmail.com>
2024-03-15 14:30:22 +0000

    WIP: Added some small changes

--- MerlinModel.c
+++ MerlinModel.c
@@ -11,21 +11,23 @@
 
 
 /* Initalizes Merlin Model struct */
-struct MerlinMachine *CreateMerlinMachine(int number_of_features, int number_of_ta_chunks, int number_of_state_bits, int T, double s) {
+struct MerlinMachine *CreateMerlinMachine(int number_of_features, int number_of_patches, int number_of_ta_chunks, int number_of_state_bits, int T, double s) {
     
     struct MerlinMachine *mm = (void *)malloc(sizeof(struct MerlinMachine));
 
     mm->number_of_features = number_of_features;
+    mm->number_of_patches = number_of_patches;
     mm->number_of_ta_chunks = number_of_ta_chunks;
     mm->number_of_state_bits = number_of_state_bits;
     mm->T = T;
     mm->s = s;
 
-
     mm->ta_state = (unsigned int *)malloc(sizeof(unsigned int) * number_of_ta_chunks * number_of_state_bits);
     mm->literal_outputs = (unsigned int *)malloc(sizeof(unsigned int) * number_of_ta_chunks);
     mm->feedback_to_la = (unsigned int *)malloc(sizeof(unsigned int) * number_of_ta_chunks);
     mm->literal_weights = (unsigned int *)malloc(sizeof(unsigned int) * number_of_features);
+    mm->output_one_patches = (int *)malloc(sizeof(int) * number_of_patches);  /* THIS IS USED FOR DECIDING WHICH STATES GET INCREMENTED*/
+    mm->literal_patch = (unsigned int*)malloc(sizeof(unsigned int*) * number_of_features);
 
     if (((number_of_features) % 32) != 0) {
 		mm->filter  = (~(0xffffffff << ((number_of_features) % 32)));
@@ -66,7 +68,7 @@ static inline void mm_inc(struct MerlinMachine *mm, int chunk, unsigned int acti
     
     unsigned int carry, carry_next;
 
-    unsigned int *ta_state = &mm->ta_state[mm->number_of_ta_chunks*mm->number_of_state_bits + chunk*mm->number_of_state_bits];
+    unsigned int *ta_state = &mm->ta_state[chunk*mm->number_of_state_bits];
 
     /* Bob: This code essentially just implements a very basic add operation in bit form
             the bit operations of AND and XOR are exactly the same as the gate implementations. Looks
@@ -105,7 +107,7 @@ static inline void mm_dec(struct MerlinMachine *mm, int chunk, unsigned int acti
 {
 	unsigned int carry, carry_next;
 
-	unsigned int *ta_state = &mm->ta_state[mm->number_of_ta_chunks*mm->number_of_state_bits + chunk*mm->number_of_state_bits];
+	unsigned int *ta_state = &mm->ta_state[chunk*mm->number_of_state_bits];
 
     /* Bob: This code works very similar to the previous increment, the major difference being that we negate the
             and causing the bit if zero to borrow from the next bit basically saying we need to decrement the next
@@ -137,6 +139,7 @@ static inline int sum_up_class_votes(struct MerlinMachine *mm) {
 
     int class_sum = 0;
 
+    // Adds up output from each literal given the literal weight
     for (int literals = 0; literals < mm->number_of_features; literals++) {
         int ta_chunk = literals / 32;
         int ta_chunk_pos = literals % 32;
@@ -149,6 +152,56 @@ static inline int sum_up_class_votes(struct MerlinMachine *mm) {
 
     return class_sum;
 }
+
+static inline void mm_calculate_literal_output(struct MerlinMachine *mm, unsigned int *Xi, int predict) {
+
+    // Number of patches that have an output of 1
+    int output_one_patches_count = 0;
+
+    int output = 0;
+
+    unsigned int *ta_state = mm->ta_state;
+
+    // Filling output array with zeros
+    for (int lit = 0; lit < mm->number_of_features; lit++) {
+        mm->literal_outputs[lit] = 0;
+    }
+
+    for (int patch = 0; patch < mm->number_of_patches; ++patch) {
+        for(int chunk = 0; chunk < mm->number_of_ta_chunks-1; chunk++) {
+            unsigned int pos_in_chunk = chunk*mm->number_of_state_bits + mm->number_of_state_bits-1;
+            output = (ta_state[pos_in_chunk] & Xi[patch*mm->number_of_ta_chunks + chunk]) == ta_state[pos_in_chunk];
+
+            if (output) {
+                
+            }
+
+        }
+
+        unsigned int pos_last_chunk = (mm->number_of_ta_chunks-1)*mm->number_of_state_bits + mm->number_of_state_bits-1;
+        output = (ta_state[pos_last_chunk] & Xi[patch*mm->number_of_ta_chunks + mm->number_of_ta_chunks - 1] & mm->filter) ==
+            (ta_state[pos_last_chunk] & mm->filter);
+
+        output = output && !(predict == PREDICT);
+
+        if (output) {
+            mm->output_one_patches[output_one_patches_count] = patch;
+            output_one_patches_count++;
+        }
+
+    }
+
+    if (output_one_patches_count > 0) {
+        unsigned int clause_chunk = j / 32;
+        unsigned int clause_chunk_pos = j % 32;
+
+        tm->clause_output[clause_chunk] |= (1 << clause_chunk_pos);
+
+        int patch_id = fast_rand() % output_one_patches_count;
+        tm->clause_patch[j] = tm->output_one_patches[patch_id];
+ 	}
+}
+
 
 
 
@@ -173,3 +226,4 @@ int mm_ta_state(struct MerlinMachine *mm, int ta) {
 
     return state;
 }
+
--- MerlinModel.h
+++ MerlinModel.h
@@ -5,13 +5,17 @@
 
 struct MerlinMachine {
     int number_of_features;
+    int number_of_patches;
 
     omp_lock_t *literal_lock;
 
     unsigned int *ta_state;
     unsigned int *literal_outputs;
     unsigned int *feedback_to_la;
+    unsigned int *literal_patch;
     
+    int *output_one_patches;
+
     unsigned int *literal_weights;
 
     int number_of_ta_chunks;
@@ -24,7 +28,7 @@ struct MerlinMachine {
     unsigned int filter;
 };
 
-struct MerlinMachine *CreateMerlinMachine(int number_of_features, int number_of_ta_chunks, int number_of_state_bits, int T, double s);
+struct MerlinMachine *CreateMerlinMachine(int number_of_features, int number_of_patches, int number_of_ta_chunks, int number_of_state_bits, int T, double s);
 
 void mm_initialize(struct MerlinMachine *mm);
 
